4e4525452f6a8adfa09ed135a1403cd2fa9668c7
Prakhar Ojha <prakharojha12@gmail.com>
2021-09-26 11:20:30 +0530

    Twilio Integration Sucessful

--- server/routes/Home.js
+++ server/routes/Home.js
@@ -1,9 +1,20 @@
+const { response } = require('express');
 const express = require('express');
 const router = express.Router();
+const getModel= require('../models/Contacts');
 
-router.get('/Home',(req,res)=>{
+router.get('/Home',async (req,res)=>{
     const signal = req.body.signal;
-    if(signal)
-    res.send('Send the Signal ASAP!');
+    if(signal=='true')
+    {
+        const userId = req.headers.userid;
+        const contactModel = getModel(userId);
+        const data = await contactModel.find({});
+        data.map((contact)=>{
+            console.log(contact.Name);
+            console.log(contact.Contact);
+        })
+    }
+    res.send('Data Done!')
 })
 module.exports = router;
--- server/routes/SendSms.js
+++ server/routes/SendSms.js
@@ -1,19 +1,22 @@
 const dotenv = require('dotenv')
-dotenv.config();
-const SID = process.env.TWILIO_SID;
-const API = process.env.TWILIO_API;
+const path = require('path')
+dotenv.config({path:path.resolve(__dirname,'../.env')});
 
-const client = require('twilio')(SID,API);
-
-const sendSMS = (message,toWho) =>{
+const sendSMS = (message, toWho) => {
+    const SID = process.env.TWILIO_SID;
+    const API = process.env.TWILIO_API;
+    const client = require('twilio')(SID, API);
     client.messages.create({
-        to:toWho,
-        from:'+16154995410',
-        body:"This sms thing really works Wow"
+        to: `${toWho}`,
+        from: '+16154995410',
+        body: `${message}`
     })
-    .then(message=>{
-        console.log(message.sid);
-    })
+        .then(message => {
+            console.log(message);
+            console.log(message.sid);
+            console.log('Messaeg');
+        })
+        .catch(errr=>console.log(errr))
 }
 
 module.exports = sendSMS;
